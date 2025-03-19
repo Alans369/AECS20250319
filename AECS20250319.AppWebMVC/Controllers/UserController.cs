f224040217557e6d7c1ecab569fb93662828502d
Alan Edenilson Campos Sanchez <cs25001@esfe.agape.edu.sv>
2025-03-19 11:05:48 -0600

    usuarios

--- AECS20250319.AppWebMVC/Controllers/UserController.cs
+++ AECS20250319.AppWebMVC/Controllers/UserController.cs
@@ -89,7 +89,7 @@ namespace AECS20250319.AppWebMVC.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("UserId,Username,Email,PasswordHash,Role")] User user)
+        public async Task<IActionResult> Edit(int id, [Bind("UserId,Username,Email,Role")] User user)
         {
             if (id != user.UserId)
             {
