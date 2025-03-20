4864e9fcec48f4143459c1660551ee6c7efd1ae3
Alan Edenilson Campos Sanchez <cs25001@esfe.agape.edu.sv>
2025-03-20 08:27:38 -0600

    terminado

--- AECS20250319.AppWebMVC/Models/Product.cs
+++ AECS20250319.AppWebMVC/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace AECS20250319.AppWebMVC.Models;
 
@@ -13,8 +14,10 @@ public partial class Product
 
     public decimal Price { get; set; }
 
+    [Display(Name = "Categoria")]
     public int? CategoryId { get; set; }
 
+    [Display(Name = "Marca")]
     public int? BrandId { get; set; }
 
     public virtual Brand? Brand { get; set; } = null!;
